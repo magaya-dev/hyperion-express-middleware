fee34468e783a44e7e5f143dee3bd15b4a0490d4
Jorge Cabot <jcabot@magaya.com>
2017-01-12 10:20:51 -0500

    Fixed connect call

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const hyperion = require('./build/hyperion');
-const dbx = hyperion.Connect('livetrack-mobile', process.argv); 
+const dbx = hyperion.connect('livetrack-mobile', process.argv); 
 
 module.exports = function (request, response, next) {
     if (!dbx) {
