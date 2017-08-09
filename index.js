bbe16f58b7195af4244215dd15f57f2c654e9de4
Jorge Cabot <jcabot@magaya.com>
2017-08-09 12:26:31 -0400

    Added dependency on hyperion-node npm package. Removed bin and hyper-api from project. Upped version number.

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-const database = require('./hyper-api');
+const hyperion = require('hyperion-node');
 
 module.exports = function (moduleName, args) {
-    const connection = database({
+    const database = hyperion({
             name: moduleName,
             argv: args
         });
     
     return function (request, response, next) {
-        if (!connection) {
+        if (!database) {
             throw new Error(`There is no connection to the database for ${moduleName}...`);
         }
 
-        request.dbx = connection.dbx;
-        request.algorithm = connection.algo;
-        request.api = connection.api;
+        request.dbx = database.dbx;
+        request.algorithm = database.algorithm;
+        request.api = database.connection.livetrack;
 
         next();
     };
