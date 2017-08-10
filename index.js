054a937a3c0cae6f637807d9b50f69e566d25db4
Jorge Cabot <jcabot@magaya.com>
2017-08-10 14:45:20 -0400

    Fix format

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ module.exports = function (moduleName, args) {
     }
 
     const database = hyperion({
-            name: moduleName,
-            argv: args
-        });
-    
+        name: moduleName,
+        argv: args
+    });
+
     return function (request, response, next) {
         if (!database) {
             throw new Error(`There is no connection to the database for ${moduleName}`);
